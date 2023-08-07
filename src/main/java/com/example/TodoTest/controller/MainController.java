34ceae687978b5e6834ceefc6e28d0c4b05f2a5e
ParkYoungGil <eclipse9128@naver.com>
2023-08-07 15:00:07 +0900

    branch change0.1

---
+++ src/main/java/com/example/TodoTest/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.example.TodoTest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping(value = "/")
+    public String rootAPI(){
+        return "To-do Application !";
+    }
+}
