29d74cf20da7a9de7e55196343458e1db552525b
ParkYoungGil <eclipse9128@naver.com>
2023-08-07 15:06:37 +0900

    1

--- src/main/java/com/example/TodoTest/controller/MainController.java
+++ src/main/java/com/example/TodoTest/controller/MainController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @GetMapping(value = "/")
     public String rootAPI(){
-        return "To-do Application !";
+        return "To-do Application !!";
     }
 }
